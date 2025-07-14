a1552025fc457702a6496a6ee9378fe63a0f357d
amanmalik1709 <aman.malik.ug21@nsut.ac.in>
2025-07-15 00:26:24 +0530

    learnt about variables in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all js code as newer version of JS 
+
+/* 
+    alert("Hello World!");  
+    We are using console not browser so aleart will not work. 
+    Alert is used in browser to show message to user as a popup.
+*/
+
+
+// number 
+// bigint 
+// string => "Hello World!"
+// boolean => true/false
+// null => standalone value, no value // also is a object
+// undefined => variable is declared but not assigned any value
+// symbol => unique and immutable value, used for object properties
+
+// object => collection of key-value pairs, can be mutable or immutable
+
+
+console.log(typeof 10); // number
+console.log(typeof null);// object 
+console.log(typeof undefined); // undefined
